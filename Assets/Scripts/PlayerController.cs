02f0b682536882d6bc2e451d9849553fdc69adad
Elliott Klug <careeningspace@gmail.com>
2020-01-22 15:36:24 -0500

    Camera Setup
    Camera follows ball, direction of camer controls movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,28 @@ public class PlayerController : MonoBehaviour
     public float turningSpeed;
 
     private Rigidbody rb;
+    private Camera cam;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //transform.Translate(Vector3.forward);
-        float moveHorizontal = -Input.GetAxis("Horizontal");
-        float moveVertical = -Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         transform.Rotate(Vector3.up, moveHorizontal);
+
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement.y = 0.0f;
 
         rb.AddForce(movement * playerSpeed);
     }
