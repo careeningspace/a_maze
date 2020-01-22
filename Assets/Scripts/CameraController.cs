e59813f40f970ed410e2054dcd4dff2c0fd99588
Elliott Klug <careeningspace@gmail.com>
2020-01-21 22:02:50 -0500

    Map Update
    Map design completed for first version.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    float distance;
+    Vector3 playerPrevPos, playerMoveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+        distance = offset.magnitude;
+        playerPrevPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        playerMoveDir = player.transform.position - playerPrevPos;
+        playerMoveDir.Normalize();
+
+        transform.position = player.transform.position - playerMoveDir * distance;
+
+        transform.LookAt(player.transform.position);
+
+        playerPrevPos = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+    public float turningSpeed;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //transform.Translate(Vector3.forward);
+        float moveHorizontal = -Input.GetAxis("Horizontal");
+        float moveVertical = -Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Rotate(Vector3.up, moveHorizontal);
+
+        rb.AddForce(movement * playerSpeed);
+    }
+}
